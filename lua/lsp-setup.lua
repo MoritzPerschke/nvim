f807d6d19c26b0a3722980d8ac0466d5b0a5aa50
Moritz Perschke <moritz.perschke@student.uibk.ac.at>
2025-01-16 16:11:28 +0100

    theoretically, these changes should enabe the use of clang tidy, right now i just put a clang-tidy file in ~

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -46,21 +46,21 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-      require('which-key').add {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      }
+require('which-key').add {
+  { '<leader>c', group = '[C]ode' },
+  { '<leader>d', group = '[D]ocument' },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>w', group = '[W]orkspace' },
+  { '<leader>t', group = '[T]oggle' },
+  { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+}
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
 require('which-key').add {
-    { "<leader>", group = "VISUAL <leader>", mode = "v" },
-    { "<leader>h", desc = "Git [H]unk", mode = "v" },
-  }
+  { "<leader>",  group = "VISUAL <leader>", mode = "v" },
+  { "<leader>h", desc = "Git [H]unk",       mode = "v" },
+}
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -75,8 +75,16 @@ require('mason-lspconfig').setup()
 --
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
+
 local servers = {
-  clangd = {},
+  clangd = {
+    cmd = {
+      -- "--clang-tidy",
+      -- "--checks=modernize-*,bugprone-*",
+      -- "--warnings-as-errors=*",
+      -- "--enable-config",
+    },
+  },
   -- gopls = {},
   pyright = {},
   -- rust_analyzer = {},
@@ -88,10 +96,11 @@ local servers = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+      diagnostics = { disable = { 'missing-fields' } },
     },
   },
 }
+vim.keymap.set('n', '<leader>di', vim.diagnostic.open_float, { desc = "Show diagnostics" })
 
 -- Setup neovim lua configuration
 require('neodev').setup()
