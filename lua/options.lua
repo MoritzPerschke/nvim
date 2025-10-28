edafb44bbe2aa5d5d788770cfccfea497e7c99e7
Moritz Perschke <perschkemoritz@gmail.com>
2025-10-28 22:41:15 +0100

    hope this works

--- lua/options.lua
+++ lua/options.lua
@@ -16,6 +16,8 @@ vim.o.mouse = 'a'
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
+-- Only for wayland sessions with wl-copy installed
+-- vim.g.clipboard = 'wl-copy'
 
 -- Enable break indent
 vim.o.breakindent = true
